3b2aa7c9300b95289ee13c8d76db672fe319d8d3
Ayush Kumar <kr.ayush998@gmail.com>
2020-02-24 00:54:53 +0530

    Added code for removing redundant nodes

--- src/parser/java8main.py
+++ src/parser/java8main.py
@@ -4,17 +4,30 @@ from java8Parser import java8Parser
 from java8Visitor import java8Visitor
 from graphviz import Digraph
 
-dot = Digraph(comment="Abstract Syntax Tree")
-
-
 class AST_Node():
-	def __init__(self,val = None, parent = None):
-		self.val = val
+	# static variable num_nodes to count the number of nodes
+	num_nodes = 0
+	nodes = []
+	def __init__(self,text = None, parent = None):
+		self.id = AST_Node.num_nodes
+		self.text = text
 		self.children = []
 		self.parent = parent
+		AST_Node.num_nodes += 1
+		AST_Node.nodes.append(self)
 
-	def add_children(node):
+	def addChild(self, node):
 		self.children.append(node)
+		node.parent = self
+
+	def getChildren(self):
+		return self.children
+	
+	def getText(self):
+		return self.text
+
+	def getId(self):
+		return self.id
 
 class AST_Graph():
 
@@ -22,30 +35,68 @@ class AST_Graph():
 		self.root = None
 
 	def add_node(self,val,parnode):
-		newnode = AST_Node(val,parnode):
+		newnode = AST_Node(val,parnode)
 		if (self.root == None):
 			self.root = newnode
 
 	def add_children(self,parnode,childnode):
 		parnode.add_children(childnode)
 
-def handleExpression(expr):
-	type_str = str(type(expr))
-	type_str = type_str.split('.')[-1][:-2]
-	print(type_str)
-	for child in expr.getChildren():
-		if isinstance(child, antlr4.tree.Tree.TerminalNode):
+def createGraph(root, dot):
+	dot.node(str(root.getId()), root.getText())
+	for child in root.getChildren():
+		createGraph(child, dot)
+		dot.edge(str(root.getId()), str(child.getId()))
+
+def deepCopy(old_root, par):
+	# creates a copy of tree using AST_Nodes
+	new_rootnode = AST_Node(old_root.getText(), par)
+	if isinstance(old_root, antlr4.tree.Tree.TerminalNode):
+		# this is a terminal node, it doesn't have any children
+		pass
+	else:
+		for child in old_root.getChildren():
+			new_rootnode.addChild(deepCopy(child, new_rootnode))
+	return new_rootnode
+
+def compressTree(root):
+	# this method removes all redundant nodes from the tree
+	# nodes which have a single child is a redundant node
+	idx = 0
+	while idx < len(root.getChildren()):
+		child = root.getChildren()[idx]
+		if len(child.getChildren()) == 0:
+			pass
+		elif len(child.getChildren()) == 1:
+			root.getChildren()[idx] = child.getChildren()[0]
+			idx -= 1
+		else:
+			compressTree(child)
+		idx += 1
+	return root
+
+def handleExpression(root):
+	print(root.getText())
+	print("visiting children...")
+	for idx, child in enumerate(root.getChildren()):
+		print(idx)
+		if len(child.getChildren()) == 0:
 			pass
 		else:
 			handleExpression(child)
 
 def main():
-    lexer = java8Lexer(antlr4.StdinStream())
-    stream = antlr4.CommonTokenStream(lexer)
-    parser = java8Parser(stream)
-    tree = parser.compilationUnit()
-    handleExpression(tree)
+	lexer = java8Lexer(antlr4.StdinStream())
+	stream = antlr4.CommonTokenStream(lexer)
+	parser = java8Parser(stream)
+	tree = parser.compilationUnit()
+	mytree = deepCopy(tree, None)
+	compressTree(mytree)
+	dot = Digraph(comment="Abstract Syntax Tree")
+	createGraph(mytree, dot)
+	dot.render('graph', view=True)
+	#handleExpression(mytree)
 
 if __name__ == '__main__':
-    main()
+	main()
 
