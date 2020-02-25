c547469f2a7738c54fb40a53e8135af9de1aeaaa
Ayush Kumar <kr.ayush998@gmail.com>
2020-02-25 21:35:36 +0530

    Added verbose option

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -39,12 +39,14 @@ public:
 int AST_Node::num_nodes = 0;
 std::vector<AST_Node*> AST_Node::nodes;
 
-AST_Node* createAST(antlr4::tree::ParseTree* root, AST_Node* par, Java8Parser& parser, Java8Lexer& lexer) {
+AST_Node* createAST(antlr4::tree::ParseTree* root, AST_Node* par, bool verbose, Java8Parser& parser, Java8Lexer& lexer) {
     AST_Node* newnode = par;
     // handling the special case of assignment
     if (dynamic_cast<antlr4::ParserRuleContext*>(root)) {
         std::string type = parser.getRuleNames()[dynamic_cast<antlr4::ParserRuleContext*>(root)->getRuleIndex()];
+        if (verbose) {std::cout << "Visiting nonterminal " + std::string(type) + "..." << std::endl;}
         if (!type.compare("assignment")) {
+            // handling the special case of assignment rule
             // this is an assignment rule hence it must have 3 children
             // also the second child should be of type assignment operator
             assert(root->children.size() == 3);
@@ -53,7 +55,7 @@ AST_Node* createAST(antlr4::tree::ParseTree* root, AST_Node* par, Java8Parser& p
             newnode = new AST_Node(root->children[1]->getText(), type, rulenumber, par);
             for (int i : {0, 2}) {
                 // processing child i
-                createAST(root->children[i], newnode, parser, lexer);
+                createAST(root->children[i], newnode, verbose, parser, lexer);
             }
             return newnode;
         }
@@ -74,19 +76,20 @@ AST_Node* createAST(antlr4::tree::ParseTree* root, AST_Node* par, Java8Parser& p
             }
         }
     }
+    int rulenumber = dynamic_cast<antlr4::ParserRuleContext*>(root)->getRuleIndex();
+	std::string type = parser.getRuleNames()[rulenumber];
+    if (is_skipnode && verbose) {std::cout << "\tPruning Tree at nonterminal " + std::string(type) + "..." << std::endl;}
     if (!is_skipnode) {
-        int rulenumber = dynamic_cast<antlr4::ParserRuleContext*>(root)->getRuleIndex();
-		std::string type = parser.getRuleNames()[rulenumber];
 		newnode = new AST_Node(root->getText(), type, rulenumber, par);
     }
 	for (antlr4::tree::ParseTree* child : root->children) {
 		if (dynamic_cast<antlr4::ParserRuleContext*>(child)) {
-			createAST(child, newnode, parser, lexer);
+			createAST(child, newnode, verbose, parser, lexer);
         } else if (dynamic_cast<antlr4::tree::TerminalNodeImpl*>(child)) {
             std::string symbolname = 
                 lexer.getVocabulary().getSymbolicName(dynamic_cast<antlr4::tree::TerminalNodeImpl*>(child)->getSymbol()->getType());
             if (!symbolname.compare("Identifier")) {
-                createAST(child, newnode, parser, lexer);
+                createAST(child, newnode, verbose, parser, lexer);
             }
         }
     }
@@ -113,25 +116,36 @@ Agnode_t* createGraph(AST_Node* root, Agraph_t* g) {
 }
 
 int main(int argc, const char* argv[]) {
+    bool verbose = true;
     std::ifstream stream;
     stream.open(argv[1]);
     antlr4::ANTLRInputStream input(stream);
+    if (verbose) {std::cout << "Parsing program..." << std::endl;}
     Java8Lexer lexer(&input);
     antlr4::CommonTokenStream tokens(&lexer);
-    Java8Parser parser(&tokens);   
+    Java8Parser parser(&tokens);
+    if (verbose) {std::cout << "Parsing complete." << std::endl;}
+    if (verbose) {std::cout << "Constructing ParseTree..." << std::endl;}
     antlr4::tree::ParseTree* tree = parser.compilationUnit();
-    AST_Node* ast = createAST(tree, NULL, parser, lexer);
+    if (verbose) {std::cout << "ParseTree successfully constructed." << std::endl;}
+    if (verbose) {std::cout << "Constructing AbstractSyntaxTree..." << std::endl;}
+    AST_Node* ast = createAST(tree, NULL, verbose, parser, lexer);
+    if (verbose) {std::cout << "AbstractSyntaxTree successfully constructed." << std::endl;}
 
     //create and render the graph
     char astgraph_name[] = "Abstract Syntax Tree";
     Agraph_t* astgraph = agopen(astgraph_name, Agstrictdirected, 0);
+    if (verbose) {std::cout << "Constructing Graph..." << std::endl;}
     Agnode_t* astgraph_root = createGraph(ast, astgraph);
+    if (verbose) {std::cout << "Graph successfully constructed." << std::endl;}
     /* set up a graphviz context */
     GVC_t* gvc = gvContext();
     /* parse command line args - minimally argv[0] sets layout engine */
     //gvParseArgs(gvc, argc, argv);
     gvLayout(gvc, astgraph, "dot");
+    if (verbose) {std::cout << "Rendering Graph..." << std::endl;}
     gvRenderFilename(gvc, astgraph, "dot", "graph.gv");
+    if (verbose) {std::cout << "Graph successfully rendered." << std::endl;}
     system("dot -Tpdf graph.gv >| graph.pdf");
     system("rm -f graph.gv");
     gvFreeLayout(gvc, astgraph);
